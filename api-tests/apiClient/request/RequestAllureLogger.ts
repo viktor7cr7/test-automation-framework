13552bd95a8381ed00c2f6ef4063bda11a2c2dd9
Victor Leonov <leonovvk98@gmail.com>
2025-07-29 22:24:11 +0300

    поправил ошибку при сравнении статусов, убрал undefined в url  при логировании реквеста в allure

--- api-tests/apiClient/request/RequestAllureLogger.ts
+++ api-tests/apiClient/request/RequestAllureLogger.ts
@@ -5,20 +5,18 @@ import { Response } from '../../../types/response/response.ts';
 
 export class RequestAllureLogger implements IAllureLogger {
   private testInfo = test.info();
-  public attachRequest<TRequest>(options: RequestOptions<TRequest>) {
-    this.testInfo.attach(`Request method ${options.method} ${options.url}`, {
+  public attachRequest<TRequest>(options: RequestOptions<TRequest>, endpoint: string) {
+    this.testInfo.attach(`Request method ${options.method} ${endpoint}`, {
       body: JSON.stringify({ headers: options.headers, ...(options.data && { body: options.data }) }, null, 2),
-      contentType: 'application/json',
     });
   }
 
-  public attachResponse<TRequest, TResponse extends object | string>(
+  public attachResponse<TRequest, TResponse extends object | string | null>(
     options: RequestOptions<TRequest>,
     response: Response<TResponse>
   ) {
     this.testInfo.attach(`response ${options.method} ${response.url}`, {
       body: JSON.stringify({ headers: response.headers, body: response.body }, null, 2),
-      contentType: 'application/json',
     });
   }
 }
--- types/allureLogger/AllureLogger.ts
+++ types/allureLogger/AllureLogger.ts
@@ -2,8 +2,8 @@ import { RequestOptions } from '../request/request.ts';
 import { Response } from '../response/response.ts';
 
 export interface IAllureLogger {
-  attachRequest<TRequest>(options: RequestOptions<TRequest>): void;
-  attachResponse<TRequest, TResponse extends object | string>(
+  attachRequest<TRequest>(options: RequestOptions<TRequest>, endpoint: string): void;
+  attachResponse<TRequest, TResponse extends object | string | null>(
     options: RequestOptions<TRequest>,
     response: Response<TResponse>
   ): void;
