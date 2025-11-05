a19d0c210d3d6cb48a2c6918b0574dd705045093
Viktor Leonov <98475796+viktor7cr7@users.noreply.github.com>
2025-11-05 17:56:34 +0300

    Update sortArrays.ts

--- ui-tests/utils/sortArrays.ts
+++ ui-tests/utils/sortArrays.ts
@@ -2,7 +2,8 @@ export function sortStringArrays(firstArray: string[], secondArray: string[]) {
   const result: string[][] = [];
 
   result.push(firstArray.sort((a, b) => a.localeCompare(b)));
-  result.push(secondArray.sort((a, b) => a.localeCompare(b)));
+  // result.push(secondArray.sort((a, b) => a.localeCompare(b)));
 
+  result.push(secondArray.sort((a, b) => a.localeCompare(b)));
   return result;
 }
